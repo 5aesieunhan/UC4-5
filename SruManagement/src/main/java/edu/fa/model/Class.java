10a2183e8967aaaf60a6237f92c97ddc7ae1176e
huynhhoang1112 <47128569+huynhhoang1112@users.noreply.github.com>
2019-01-29 15:38:19 +0700

    Chỉnh sửa file Class.class

--- SruManagement/src/main/java/edu/fa/model/Class.java
+++ SruManagement/src/main/java/edu/fa/model/Class.java
@@ -1,6 +1,7 @@
 package edu.fa.model;
 
 import java.sql.Date;
+import java.util.List;
 
 public class Class {
 	private int classID;
@@ -8,6 +9,56 @@ public class Class {
 	private Date openDate;
 	private String note;
 	private int trainerID;
+	private List<Trainee> listTrainee;
+	public Class(int classID, String className, Date openDate, String note, int trainerID, List<Trainee> listTrainee) {
+		super();
+		this.classID = classID;
+		this.className = className;
+		this.openDate = openDate;
+		this.note = note;
+		this.trainerID = trainerID;
+		this.listTrainee = listTrainee;
+	}
+	public Class() {
+		super();
+	}
+	public int getClassID() {
+		return classID;
+	}
+	public void setClassID(int classID) {
+		this.classID = classID;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public Date getOpenDate() {
+		return openDate;
+	}
+	public void setOpenDate(Date openDate) {
+		this.openDate = openDate;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public int getTrainerID() {
+		return trainerID;
+	}
+	public void setTrainerID(int trainerID) {
+		this.trainerID = trainerID;
+	}
+	public List<Trainee> getListTrainee() {
+		return listTrainee;
+	}
+	public void setListTrainee(List<Trainee> listTrainee) {
+		this.listTrainee = listTrainee;
+	}
+	
 	
 	
 }
